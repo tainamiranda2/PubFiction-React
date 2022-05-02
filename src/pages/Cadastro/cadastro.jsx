3338848b76766d4607bcb764da241f3dd25a3349
Tainá Miranda <tainamirandaferreira@gmail.com>
2022-05-02 15:20:17 -0300

    Vendo

--- src/pages/Cadastro/cadastro.jsx
+++ src/pages/Cadastro/cadastro.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from '../../components/button/Button';
 import Inputs from '../../components/inputs/Inputs';
+import * as C from './style';
+
 export const Cadastro=()=>{
 
 
   return (
-    <>
-    <h1>Olá, aqui é a tela de cadastro</h1>
+    <C.Container>
+    <h1>Olá, faça seu cadastro e aproveite.</h1>
  <Inputs 
  name="Nome" 
  placeholder="Nome completo"
@@ -15,8 +17,31 @@ text="Nome"
  
  />
 
+<Inputs 
+ name="Email" 
+ placeholder="Email"
+ type="email"
+text="Email"
+ 
+ />
+
+<Inputs 
+ name="Senha" 
+ placeholder="Confirme sua senha"
+ type="password"
+text="Senha*"
+ 
+ />
+
+<Inputs 
+ name="Senha" 
+ placeholder="Senha"
+ type="password"
+text="Senha"
+ 
+ />
 <Button text="Enviar" to="/"/>
 
-    </>
+    </C.Container>
   )
 }
--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../../components/button/Button';
-
+import Inputs from '../../components/inputs/Inputs';
+import * as C from './style';
 
 export const Login=()=>{
  
@@ -18,17 +19,44 @@ function enviardados() {
 
 
   return (
-    <>
-    <h1>Faça login</h1>
-    <span>Esqueceu a senha?</span>
+    <C.Container>
+    <h1>Faça login e aproveite a plataforma.</h1>
     <form onSubmit={enviardados} name="dados"> 
-    
+    <Inputs 
+ name="Nome" 
+ placeholder="Nome completo"
+ type="text"
+text="Nome"
+ 
+ />
 
+<Inputs 
+ name="Email" 
+ placeholder="Email"
+ type="email"
+text="Email"
+ 
+ />
+ <Inputs 
+ name="Nome" 
+ placeholder="Nome completo"
+ type="text"
+text="Nome"
+ 
+ />
 
-
+<Inputs 
+ name="Senha" 
+ placeholder="Senha"
+ type="password"
+text="Senha"
+ 
+ />
 </form>
+<span>Esqueceu a senha?</span>
+
 <Button handleOnChangeButton={button} text="Enviar"/>
 
-    </>
+    </C.Container>
   )
 }
